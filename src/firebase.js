8e50f3e94569aa8f24203216d03a1c177aab6709
tasnimsimum <tasnimsimum@gmail.com>
2025-10-09 04:36:45 +0000

    Merge conflicting files

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getFunctions, connectFunctionsEmulator, httpsCallable } from 'firebase/functions';
 
 // Firebase configuration from environment variables
 const firebaseConfig = {
@@ -21,6 +22,19 @@ export const auth = getAuth(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+
+// Initialize Cloud Functions and get a reference to the service
+export const functions = getFunctions(app, 'us-central1');
+
+if (window.location.hostname === "localhost") {
+  // Point to the emulators
+  connectAuthEmulator(auth, "http://localhost:9099");
+  connectFirestoreEmulator(db, "localhost", 8080);
+  connectFunctionsEmulator(functions, "localhost", 5001);
+}
+
+
+export const chat = httpsCallable(functions, 'chat');
 
 // Export app for Remote Config and Analytics
 export { app };
