2eb1121ffd497ffa436cea82bdd0f79bee8ad936
stasnim10 <tasnimsimum@gmail.com>
2025-10-09 17:59:15 -0400

    fix: revert to Tailwind v3 with safelist and design tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  safelist: [
+    { pattern: /bg-(yellow|orange|purple|indigo|blue|green|red)-(50|100|200|500)/ },
+    { pattern: /text-(yellow|orange|purple|indigo|blue|green|red)-(400|500|600)/ }
+  ],
+  plugins: []
+};
